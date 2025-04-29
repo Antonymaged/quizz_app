31c504502c10237bad6e9a816cb9bbda02e517d3
AymanIshaq1 <aymaneshaaq@gmail.com>
2025-04-29 21:28:39 +0300

    update home

---
+++ js/home.js
@@ -0,0 +1,20 @@
+function gotoprofile() {
+    window.location.href = "profile.html";
+}
+
+function login() {
+    window.location.href = "login&signup.html";
+}
+function gotomodelb(){
+    window.location.href = "model_B.html";
+}
+
+function gotomodelc(){
+    window.location.href = "model_C.html";
+}
+function gotoquiz(){
+    window.location.href = "Quiz.html";
+}
+function gotoResult(){
+    window.location.href = "Quiz_Final_Result.html";
+}
